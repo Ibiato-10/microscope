ed25afcb2c351fdaadc155c6d19d324026237c64
Ibiato <ibiato-10@gmail.com>
2014-12-23 01:41:34 +0300

    adds fixtures and publications

--- client/views/posts/posts_list.js
+++ client/views/posts/posts_list.js
@@ -1,20 +1,5 @@
-var postsData = [
-  {
-    title: 'introducing Telescope',
-    author: 'Majid Al Otaibi',
-    url: 'http://icompusys.com/introducing-telescope'
-  },
-  {
-    title: 'Meteor',
-    author: 'Tom Coleman',
-    url: 'http://meteor.com'
-  },
-  {
-    title: 'The Meteor Book',
-    author: 'Tom Coleman',
-    url: 'http://themeteorbook.com'
-  }
-];
 Template.postsList.helpers({
-  posts: postsData
+  posts:function() {
+    return Posts.find();
+  }
 });
